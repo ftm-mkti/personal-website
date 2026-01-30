afe25860bc1ab6d66dcbee65c0d97fd3d0b8620d
ftm - mkti <ftm.mkti.developermail@gmail.com>
2026-01-31 01:14:46 +0330

    complete resume section

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,21 @@
 const navToggleIcon = document.querySelector('.burger-menu')
-// const menuLine = document.querySelector('.burger-menu__line')
 const menu = document.querySelector('.menu')
 const cover = document.querySelector('.cover')
+const resumeListItems = document.querySelectorAll('.resume-list__item')
 
-
-navToggleIcon.addEventListener('click', function () {
+navToggleIcon.addEventListener('click',function (){
     this.classList.toggle('nav__toggle-icon--open')
     menu.classList.toggle('menu--open')
     cover.classList.toggle('cover--show')
+})
+
+console.log("resumeListItems" + resumeListItems)
+resumeListItems.forEach(resumeListItem => {
+    resumeListItem.addEventListener('click',function (){
+        document.querySelector('.resume-list__item--active').classList.remove('resume-list__item--active')
+        document.querySelector('.resume-content--show').classList.remove('resume-content--show')
+        this.classList.add('resume-list__item--active')
+        let contentId = this.getAttribute('data-content-id')
+        document.querySelector(contentId).classList.add('resume-content--show')
+    })
 })
