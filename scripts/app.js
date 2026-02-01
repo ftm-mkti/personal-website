53b2e6296464620951edd5e9bdc5d14cf959083b
ftm - mkti <ftm.mkti.developermail@gmail.com>
2026-02-01 23:45:58 +0330

    complete portfolio section

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ const navToggleIcon = document.querySelector('.burger-menu')
 const menu = document.querySelector('.menu')
 const cover = document.querySelector('.cover')
 const resumeListItems = document.querySelectorAll('.resume-list__item')
+const portfolioListItems = document.querySelectorAll('.portfolio-list__item');
 
 navToggleIcon.addEventListener('click',function (){
     this.classList.toggle('nav__toggle-icon--open')
@@ -9,13 +10,19 @@ navToggleIcon.addEventListener('click',function (){
     cover.classList.toggle('cover--show')
 })
 
-console.log("resumeListItems" + resumeListItems)
-resumeListItems.forEach(resumeListItem => {
-    resumeListItem.addEventListener('click',function (){
-        document.querySelector('.resume-list__item--active').classList.remove('resume-list__item--active')
-        document.querySelector('.resume-content--show').classList.remove('resume-content--show')
-        this.classList.add('resume-list__item--active')
-        let contentId = this.getAttribute('data-content-id')
-        document.querySelector(contentId).classList.add('resume-content--show')
+function navigationTabsInit(listItems, listItemActiveClass, contentItemShowClass) {
+    listItems.forEach(listItem => {
+        listItem.addEventListener('click', function () {
+            removeActiveClass(listItemActiveClass);
+            removeActiveClass(contentItemShowClass);
+            this.classList.add(listItemActiveClass);
+            let contentId = this.getAttribute('data-content-id');
+            document.querySelector(contentId).classList.add(contentItemShowClass);
+        })
     })
-})
+}
+function removeActiveClass(className) {
+    document.querySelector(`.${className}`).classList.remove(className);
+}
+navigationTabsInit(resumeListItems, 'resume-list__item--active', 'resume-content--show');
+navigationTabsInit(portfolioListItems, 'portfolio-list__item--active', 'portfolio-content--show');
